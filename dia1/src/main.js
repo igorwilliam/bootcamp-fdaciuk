10df45aa5e5251dae1abe495f831dfac376ad9e1
Igor William <igorwm9@gmail.com>
2021-08-17 10:40:14 -0300

    solve exercice 04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,14 @@
 import './style.css'
 
-document.querySelector('div[data-js="app"]').innerHTML = `
+const app = document.querySelector('div[data-js="app"]');
+const link = document.querySelector('[data-js="link"]');
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+link.addEventListener('click', (event) => {
+  event.preventDefault();
+  app.hidden = !app.hidden;
+})
