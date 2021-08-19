a19b6810293859b97e2d0feb088d4ed0bea949a2
Igor William <igorwm9@gmail.com>
2021-08-19 11:42:51 -0300

    Solve exercice 02

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,4 +1,5 @@
 const inputName = document.querySelector('[data-js="name"]');
+const inputColor = document.querySelector('[data-js="color"]');
 
 inputName.addEventListener('input', (e) =>
   e.target.value = e.target.value.split(" ").map(word => {
@@ -8,5 +9,20 @@ inputName.addEventListener('input', (e) =>
     return word.charAt(0).toUpperCase() + word.substr(1);
   }).join(' ')
 )
+
+inputColor.addEventListener('change', (e) => {
+  const colors = ([...e.target.options].map(option => option.value))
+  const selectedColors = ([...e.target.selectedOptions].map(option => option.value))
+
+  colors.forEach(color => {
+    const divColor = document.querySelector(`[data-js="${color}"]`);
+
+    if (selectedColors.includes(color)) {
+      divColor.setAttribute('style', 'display: block');
+    } else {
+      divColor.setAttribute('style', 'display: none');
+    }
+  });
+})
 
 
